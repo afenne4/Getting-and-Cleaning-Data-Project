6ad5e7e83f7551fcd9d537cb9be078643bd096ab
afenne4 <afenne4@gmail.com>
2021-12-13 17:05:56 -0500

    First part of project. Reads in data and the merges files so columns are subject number, lables, and feature vector (561 long)

---
+++ run_analysis.R
@@ -0,0 +1,18 @@
+fileurl<-"https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+if(!file.exists("watchdata.zip")){
+    download.file(fileurl,"watchdata.zip",method="curl")
+    unzip("watchdata.zip")}
+X_test<-read.table("UCI HAR Dataset/test/X_test.txt")
+Y_test<-read.table("UCI HAR Dataset/test/y_test.txt")
+S_test<-read.table("UCI HAR Dataset/test/subject_test.txt")
+X_train<-read.table("UCI HAR Dataset/train/X_train.txt")
+Y_train<-read.table("UCI HAR Dataset/train/y_train.txt")
+S_train<-read.table("UCI HAR Dataset/train/subject_train.txt")
+
+#### Part 1 Merge the training and test sets ####
+Train<-cbind(S_train,Y_train,X_train)
+Test<-cbind(S_test,Y_test,X_test)
+
+
+
+#### Part 2 
